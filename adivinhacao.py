8e5d4f57884fbb173d4cb64dadff6340774bdd99
Gustavo Felipe Marques de Sousa <121033022+GustavoFMS@users.noreply.github.com>
2022-12-20 09:31:29 -0400

    Add files via upload

---
+++ adivinhacao.py
@@ -0,0 +1,69 @@
+import random
+
+bem_vindo = "Bem vindo ao jogo da adivinhacao"
+
+print("\n")
+print("*"*40)
+print(bem_vindo.center(40))
+print("*"*40)
+
+total_tentativas = 0
+resposta = 'S'
+pontuacao_total = 0
+pontos = 0
+
+while(resposta != "N"):
+    numero_adivinhacao = random.randrange(1,101)
+    print(numero_adivinhacao)
+    print("• Qual nivel de dificuldade?")
+    print("  (1) Facil (2) Medio (3) Dificil")
+
+    nivel = int(input("  → Qual nivel deseja: "))
+    validacao = 0
+
+    while(validacao != 1):
+        if(nivel == 1):
+            total_tentativas = 20
+            pontos = 25
+            validacao = 1
+        elif(nivel == 2):
+            total_tentativas = 10
+            validacao = 1
+            pontos = 50
+        elif(nivel == 3):
+            total_tentativas = 5
+            pontos = 100
+            validacao = 1
+        else:
+            print("\nValor incorreto, digite novamente!")
+            nivel = int(input("→ Qual nivel deseja: "))
+
+    for rodada in range(1, total_tentativas + 1):
+        print(f'\n• Tentativa {rodada} de {total_tentativas}')
+        chute = int(input('Digite o numero de chute: '))
+        while(total_tentativas!=0):
+            if(chute > numero_adivinhacao):
+                print("O seu chute é maior que o numero")
+                total_tentativas = total_tentativas-1
+                break
+            elif(chute < numero_adivinhacao):
+                print("O seu chute é menor que o numero")
+                break
+            elif(chute == numero_adivinhacao):
+                print(f"\n♥ Parabens, você acertou!\n• O numero a ser adivinhado era: {numero_adivinhacao}")
+                pontuacao_total = pontuacao_total + pontos;
+                resposta = str(input("→ Você deseja continuar? [S] / [N]: "))
+                if (resposta == 'S'):
+                    print(f"Pontuacao atual: {pontuacao_total}\n\n")
+                    break
+                elif(resposta == 'N'):
+                    print(f"\n\nGame Over!\nPontuação final: {pontuacao_total}")
+                    exit()
+                else: 
+                    print("\nValor incorreto, digite apenas [S] / [N]")
+                    resposta = str(input("→ Você deseja continuar? [S] / [N]: "))
+            break
+        break
+    
+print("*"*40)
+print("*"*40)
